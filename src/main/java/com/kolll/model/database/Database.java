cc2e6705b5ed869fd786479ec2ae48d7e9894468
Kolll2 <39923388+Kolll2@users.noreply.github.com>
2020-06-07 00:52:09 +0400

    added all necessary queries

--- src/main/java/com/kolll/model/database/Database.java
+++ src/main/java/com/kolll/model/database/Database.java
@@ -59,25 +59,64 @@ public class Database {
      */
 
     public List<String> getCities() throws SQLException {
+
         String part1 = "SELECT city_id, name FROM cities";
         List<String> result = new ArrayList<String>();
         resultSet = statement.executeQuery(part1);
-        while (resultSet.next()){
+        while (resultSet.next()) {
             result.add(resultSet.getInt(1) + ":" + resultSet.getString(2));
+        }
+
+        return result;
+    }
+
+    public List<String> getDistances() throws SQLException {
+        String part1 = "SELECT distance_id, from_city.name AS fromCity, cities.name AS toCity, distance FROM distances" +
+                " LEFT JOIN cities AS from_city ON distances.from_city=city_id" +
+                " LEFT JOIN cities ON distances.to_city = cities.city_id";
+        List<String> result = new ArrayList<>();
+        resultSet = statement.executeQuery(part1);
+        while (resultSet.next()) {
+            result.add(resultSet.getInt(1) + ":" + resultSet.getString(2) +
+                    " => " + resultSet.getString(3) + " ... " + resultSet.getInt(4));
         }
         return result;
     }
 
+
     public String getDistance(String fromCity, String toCity) throws SQLException {
-        return null;
+        Integer result = 0;
+        String part1 = "SELECT distance FROM distances" +
+                " LEFT JOIN cities AS from_city ON distances.from_city=city_id" +
+                " LEFT JOIN cities ON distances.to_city = cities.city_id" +
+                " WHERE from_city.name=\'" + fromCity + "\' AND cities.name=\'" + toCity+"\'";
+        resultSet = statement.executeQuery(part1);
+        if (resultSet.next())
+            result = resultSet.getInt(1);
+
+        return result.toString();
     }
 
-    public String[] getPos(String city) throws SQLException {
-        return null;
+    public List<Float> getPos(String city) throws SQLException {
+        List<Float> result = new ArrayList<>();
+        String part1 = "SELECT latitude, latitude FROM cities" +
+                " WHERE cities.name=\'" + city +"\'";
+        resultSet = statement.executeQuery(part1);
+        if (resultSet.next()){
+            result.add(resultSet.getFloat(1));
+            result.add(resultSet.getFloat(2));
+        }
+        return result;
     }
 
     private int getCityIdByName(String city) throws SQLException {
-        return 0;
+        int result = 0;
+        String part1 = "SELECT city_id FROM cities" +
+                " WHERE cities.name=\'" + city +"\'";
+        resultSet = statement.executeQuery(part1);
+        if (resultSet.next())
+            result = resultSet.getInt(1);
+        return result;
     }
 
     public void insertCities(List<City> cities) throws SQLException {
@@ -92,11 +131,22 @@ public class Database {
         for (City city : cities) {
             query.append(String.format(part2, city.getName(), city.getLatitude(), city.getLongitude()));
         }
-        query.replace(query.lastIndexOf(","),query.length(),";");
+        query.replace(query.lastIndexOf(","), query.length(), ";");
         statement.execute(query.toString());
     }
 
     public void insertDistances(List<Distance> distances) throws SQLException {
+        System.out.println("=========================================");
+        System.out.println("insertDistances(List<Distance> distances)");
+        System.out.println(Arrays.toString(distances.toArray()));
 
+        StringBuffer query = new StringBuffer("INSERT INTO distances (from_city, to_city, distance) VALUES ");
+        String part2 = "(\"%s\", \"%s\", \"%s\"),";
+
+        for (Distance distance : distances) {
+            query.append(String.format(part2, distance.getFromCity(), distance.getToCity(), distance.getDistance()));
+        }
+        query.replace(query.lastIndexOf(","), query.length(), ";");
+        statement.execute(query.toString());
     }
 }
