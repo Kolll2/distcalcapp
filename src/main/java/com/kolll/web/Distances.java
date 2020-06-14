af360cd62ba8c07cd61059ad8c6c4ddcb80af87a
Kolll2 <39923388+Kolll2@users.noreply.github.com>
2020-06-14 17:23:57 +0400

    refac: simplifying the Distance class

--- src/main/java/com/kolll/web/Distances.java
+++ src/main/java/com/kolll/web/Distances.java
@@ -14,94 +14,14 @@ import java.util.*;
 public class Distances {
 
     @GET
-    @Path("get/{from}/{to}")
-    public Response getDistance(@PathParam("from") String from, @PathParam("to") String to) {
-        return getResponseDistance(from, to);
-    }
-
-    @POST
-    @Path("get/{from}/{to}")
-    public Response postDistance(@PathParam("from") String from, @PathParam("to") String to) {
-        return getResponseDistance(from, to);
-    }
-
-    private Response getResponseDistance(String from, String to) {
-        Integer result = -1;
-        try {
-            result = DataAccessService.getDistance(from, to);
-            if (result == -1)
-                throw new NoCityInDatabaseException();
-        } catch (NoCityInDatabaseException e) {
-            return Response.status(400).entity("this route is not in the database").build();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return Response.status(200).entity(result.toString()).build();
-    }
-
-    @GET
-    @Path("crow/{from}/{to}")
-    public Response getCrow(@PathParam("from") String from, @PathParam("to") String to) {
-        try {
-            return getResponseCrow(from, to);
-        } catch (NoCityInDatabaseException e) {
-            return Response.status(400).entity("coordinates of these cities are not available").build();
-        }
-    }
-
-    @POST
-    @Path("crow/{from}/{to}")
-    public Response postCrow(@PathParam("from") String from, @PathParam("to") String to) {
-        try {
-            return getResponseCrow(from, to);
-        } catch (NoCityInDatabaseException e) {
-            return Response.status(400).entity("coordinates of these cities are not available").build();
-        }
-    }
-
-    private Response getResponseCrow(String from, String to) throws NoCityInDatabaseException {
-        Integer result = -1;
-        result = CalculationService.getCrow(from, to);
-        return Response.status(200).entity(result.toString()).build();
-    }
-
-    @GET
-    @Path("all/{from}/{to}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getAll(@PathParam("from") String from, @PathParam("to") String to) {
-        Map<String, Integer> result = new HashMap<>();
-        try {
-            result.put("Crowflight", (Integer) getResponseCrow(from, to).getEntity());
-            result.put("Distance Matrix", (Integer) getResponseDistance(from, to).getEntity());
-        } catch (NoCityInDatabaseException e) {
-            return Response.status(400).entity("coordinates of these cities are not available").build();
-        }
-        return Response.status(200).entity(result).build();
-    }
-
-    @POST
-    @Path("all/{from}/{to}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response postAll(@PathParam("from") String from, @PathParam("to") String to) {
-        Map<String, Integer> result = new HashMap<>();
-        try {
-            result.put("Crowflight", (Integer) getResponseCrow(from, to).getEntity());
-            result.put("Distance Matrix", (Integer) getResponseDistance(from, to).getEntity());
-        } catch (NoCityInDatabaseException e) {
-            return Response.status(400).entity("coordinates of these cities are not available").build();
-        }
-        return Response.status(200).entity(result).build();
-    }
-
-    @GET
-    @Path("get/{type}/{from}/{to}")
+    @Path("{type}/{from}/{to}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getType(@PathParam("type") String type, @PathParam("from") String from, @PathParam("to") String to) {
         return getResponseAll(type, from, to);
     }
 
     @POST
-    @Path("get/{type}/{from}/{to}")
+    @Path("{type}/{from}/{to}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response postType(@PathParam("type") String type, @PathParam("from") String from, @PathParam("to") String to) {
         return getResponseAll(type, from, to);
